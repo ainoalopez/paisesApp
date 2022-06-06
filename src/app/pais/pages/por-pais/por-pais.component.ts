45d3d6de30e4f14f6ebb8a01becb44420b77735a
ainoalopez <ainoalopez@iti.es>
2022-06-06 14:25:32 +0200

    llenar tabla de paises

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,24 +1,34 @@
 import { Component } from '@angular/core';
 import { PaisService } from '../../services/pais.service';
 
+import { Country } from '../../interfaces/pais.interface';
+
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
   styles: [
   ]
 })
+
 export class PorPaisComponent {
 
-  termino: string = '';
+  termino : string = '';
+  hayError: boolean = false;
+  paises  : Country[] = [];
 
   constructor( private paisService: PaisService ) { }
 
   buscar() {
+    this.hayError = false; 
     console.log( this.termino );
 
     this.paisService.buscarPais( this.termino )
-      .subscribe( resp => {
-        console.log( resp );
+      .subscribe( ( paises ) => {
+        console.log( paises ); 
+        this.paises = paises;
+    }, (err) => {
+      this.hayError = true; // si hay error se mostrará un mensaje
+      this.paises = [];
     });
   }
 
--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -1,20 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { Country } from '../interfaces/pais.interface';
+// import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaisService {
 
-  private apiUrl: string = 'https://restcountries.com/v3.1'
+  private apiUrl: string = 'https://restcountries.com/v3.1';
 
   constructor( private http: HttpClient ) { }
 
-  buscarPais( termino: string ): Observable<any> {
+  buscarPais( termino: string ): Observable<Country[]> {
 
     const url = `${ this.apiUrl }/name/${ termino }`;
-    return this.http.get( url );
-
+    return this.http.get<Country[]>( url ); // regresa un observable de tipo Country(interface)
+    // .pipe(
+    //   catchError( err => of([])) // el of es una función que genera observables y transforma los que haya dentro de los paréntesis en el observable
+    //   );
   }
 }
