e70be971a91aea958331ea2c3ce3604dfd0ab8b3
ainoalopez <ainoalopez@iti.es>
2022-06-07 08:51:36 +0200

    deBouncer

--- src/app/pais/components/pais-input/pais-input.component.ts
+++ src/app/pais/components/pais-input/pais-input.component.ts
@@ -1,18 +1,38 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-pais-input',
   templateUrl: './pais-input.component.html',
   styleUrls: ['./pais-input.component.css']
 })
-export class PaisInputComponent  {
+export class PaisInputComponent implements OnInit {
 
-  @Output() onEnter: EventEmitter<string> = new EventEmitter();
+  @Output() onEnter   : EventEmitter<string> = new EventEmitter();
+  @Output() onDebounce: EventEmitter<string> = new EventEmitter();
+
+  deBouncer: Subject<string> = new Subject();; // el Subject es un observable
 
   termino: string = '';
 
+  ngOnInit(): void {
+    // debounceTime -> no se emitirá el subscribe hasta que se deje de emitir un valor durante unos 300 milisegundos
+    this.deBouncer
+        .pipe( debounceTime ( 300 ) )
+        .subscribe( valor => {
+        this.onDebounce.emit( valor ); // también se podría utilizar el término
+    })
+  }
+
   buscar() {
     this.onEnter.emit( this.termino );
+  }
+
+  teclaPresionada() {
+    // conectamos el deBouncer y cada vez que alguien presione una tecla llamamos al next que llama al siguiente valor que es el término
+    // el next está suscrito  en el ngOnInit que llama al valor
+    this.deBouncer.next( this.termino );
   }
 
 }
