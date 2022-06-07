d9f78dc34a4746622be7f26bf52b2fba13a768e8
ainoalopez <ainoalopez@iti.es>
2022-06-07 12:06:20 +0200

    fin de la seccion 8!

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PaisService } from '../../services/pais.service';
-import { switchMap } from "rxjs/operators";
+import { switchMap, tap } from "rxjs/operators";
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-ver-pais',
@@ -11,20 +12,19 @@ import { switchMap } from "rxjs/operators";
 })
 export class VerPaisComponent implements OnInit {
 
+  pais!: Country; // puede ser que país sea nulo
+
   constructor( 
     private activetedRoute: ActivatedRoute,
     private paisService: PaisService
   ) {} // para suscribirnos a cualquier cambio del url
 
   ngOnInit(): void {
-    
     // obtenemos el id del pais
     this.activetedRoute.params
       .pipe(
-        switchMap( ({ id }) => this.paisService.getPaisPorAlpha( id ))
-      )
-      .subscribe( resp => {
-        console.log( resp );
-      });
+        switchMap( ({ id }) => this.paisService.getPaisPorAlpha( id ) ),
+        tap( console.log ) )
+      .subscribe( pais => this.pais = pais[0]); // debe ser igual a un arreglo
   }
 }
