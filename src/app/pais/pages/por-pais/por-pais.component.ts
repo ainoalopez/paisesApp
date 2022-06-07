a4a05dc35f6a16787999befc1300fa0c8271271d
ainoalopez <ainoalopez@iti.es>
2022-06-07 08:25:44 +0200

    enviar el termino del componente hijo input al componente padre por-pais

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -18,11 +18,11 @@ export class PorPaisComponent {
 
   constructor( private paisService: PaisService ) { }
 
-  buscar() {
+  buscar( termino: string ) {
     this.hayError = false; 
-    console.log( this.termino );
+    this.termino = termino;
 
-    this.paisService.buscarPais( this.termino )
+    this.paisService.buscarPais( termino )
       .subscribe( ( paises ) => {
         console.log( paises ); 
         this.paises = paises;
